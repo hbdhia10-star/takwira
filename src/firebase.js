03fabf8bb91ecc304d760020c5b3911142321468
hbdhia <hbdhia10@gmail.com>
2026-08-04 02:51:48 +0200

    Remove unused Firebase analytics

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
